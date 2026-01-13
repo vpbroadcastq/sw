1fb4992e565f33281e4eda8c7706a01d9b00dfb2
Ben Knowles <ben@ionophore.com>
2026-01-12 18:27:31 -0800

    Touchups

--- csharp/src/Sw.Cli/Program.cs
+++ csharp/src/Sw.Cli/Program.cs
@@ -1,10 +1,7 @@
-using System.Globalization;
 using System.Diagnostics;
 
 class Program
 {
-    // TODO:  Error handling for bad command lines.  For example, if you run with "dotnet run timer 2" it
-    // runs in nameless mode but the user probably wanted a new timer named "timer 2" but forgot the quotes.
     static void Main(string[] args)
     {
         DateTimeOffset programStartTime = DateTimeOffset.UtcNow;
